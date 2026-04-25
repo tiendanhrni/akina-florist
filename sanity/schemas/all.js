d5f2af10981a266a922446da0c0b62656c25c014
tiendanhrni <141383044+tiendanhrni@users.noreply.github.com>
2026-04-25 21:45:13 +0700

    Update all.js

--- sanity/schemas/all.js
+++ sanity/schemas/all.js
@@ -1,21 +1,15 @@
 import product from './product'
+import blog from './blog'
 import siteSettings from './siteSettings'
 import {
-  category,
-  banner,
-  brand,
-  gallery,
-  whySection,
-  aboutPage,
-  workshop,
-  project,
+  category, banner, brand, gallery,
+  whySection, aboutPage, workshop, project,
 } from './index'
 
 export const schemaTypes = [
-  // Cài đặt chung (singleton)
   siteSettings,
-  // Nội dung
   product,
+  blog,
   category,
   banner,
   brand,
