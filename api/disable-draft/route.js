3948e9fb518ce1cb8128b860da592bed955dee79
tiendanhrni <141383044+tiendanhrni@users.noreply.github.com>
2026-04-25 18:05:42 +0700

    Delete api directory

--- api/disable-draft/route.js
+++
@@ -1,9 +0,0 @@
-// app/api/disable-draft/route.js
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-export async function GET() {
-  const draft = await draftMode()
-  draft.disable()
-  redirect('/')
-}
